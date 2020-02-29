69d46f595ce896ab57a4b2339e0305fb647e5807
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-02-29 23:53:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+  # attr_accessor :roster
+  
+  def initialize(roster)
+    @roster = roster
+  end
+  
+  def add_student
+
+  end
+  
+  def grade
+    
+  end
+  
+  def sort
+    
+  end
+  
+  
+end
