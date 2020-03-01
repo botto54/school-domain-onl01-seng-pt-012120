2d230eb86397a6038fd7b218b8ed1ac3bf0815ea
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-01 00:29:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 # code here!
 class School
   
-  attr_accessor :roster, :student_name, :school_name
+  # attr_accessor :roster, :student_name, :school_name
   
   def initialize(school_name)
     @school_name = school_name
@@ -9,7 +9,7 @@ class School
   end
   
   def roster
-    
+    @roster
   end
   
   def add_student(student_name, grade)
